b5f0f6d82bf9bc3ec7d418e10f9e1513e0c2ef60
chantra <chantr4@gmail.com>
2016-12-19 15:32:36 -0800

    [dnscrypt] use configure.ac
    Also update travis to run tests.

--- src/wrapper/gen-version.sh
+++ src/wrapper/gen-version.sh
@@ -3,8 +3,8 @@
 
 version_file=version.h
 
-if test -d .git; then
-    version=$(git describe --always --match "v[0-9]*" | sed -r -e 's/-([^-]+)$/.\1/g' | sed -e 's/^v//g')
+if git rev-parse --is-inside-work-tree >/dev/null 2>&1; then
+    version=$(git describe --always --match "[0-9]\.*" | sed -r -e 's/-([^-]+)$/.\1/g' | sed -e 's/^v//g')
     if test -f "$version_file"; then
         VC=$(perl -ne 'print "$1" if m/.*the_version = "(.*)".*/' $version_file)
     else
